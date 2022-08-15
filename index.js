faefdd9658c2daf58249ca53a171970096c8fa59
Issam Blcayd <109247531+GUNTER404@users.noreply.github.com>
2022-08-15 13:23:05 +0000

    update a code

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ import tools from './config.js'
 
 let apkcombo = {
   search: async function(args) {
-    let res = (await fetch(tools.Proxy(tools.api(1, '/search/' + encodeURIComponent(args.replace(' ','-'))))))
+    let res = (await fetch(tools.Proxy(tools.api(1, '/search/' + encodeURIComponent(args.replace(' ', '-'))))))
     res = (await res.text())
     let $ = cheerio.load(res)
     let link = []
@@ -18,7 +18,7 @@ let apkcombo = {
     $('div.content-apps > a').each(function(a, b) {
       let nem = $(b).attr('title')
       nama.push(nem)
-      link.push($(b).attr('href').replace('https://apkcombo-com.translate.goog/','https://apkcombo.com/').replace('/?_x_tr_sl=en&_x_tr_tl=fr&_x_tr_hl=en&_x_tr_pto=wapp',''))
+      link.push($(b).attr('href').replace('https://apkcombo-com.translate.goog/', 'https://apkcombo.com/').replace('/?_x_tr_sl=en&_x_tr_tl=fr&_x_tr_hl=en&_x_tr_pto=wapp', ''))
     })
     return {
       link,
@@ -32,12 +32,17 @@ let apkcombo = {
     let img = $('div.app_header.mt-14 > div.avatar > img').attr('data-src')
     let developer = $('div.container > div > div.column.is-main > div.app_header.mt-14 > div.info > div.author > a').html()
     let appname = $('div.container > div > div.column.is-main > div.app_header.mt-14 > div.info > div.app_name > h1').text()
-    let link1 = 'https://apkcombo.com'+$('div.container > div > div.column.is-main > div.button-group.mt-14.mb-14.is-mobile-only > a').attr('href')
+    let link1 = 'https://apkcombo.com' + $('div.container > div > div.column.is-main > div.button-group.mt-14.mb-14.is-mobile-only > a').attr('href')
     res = (await fetch(link1))
     res = (await res.text())
     $ = cheerio.load(res)
-    let link = $('#best-variant-tab > div:nth-child(1) > ul > li > ul > li > a').attr('href')+'&fp=945d4e52764ab9b1ce7a8fba0bb8d68d&ip=160.177.72.111'
-    return {img , developer ,appname , link}
+    let link = $('#best-variant-tab > div:nth-child(1) > ul > li > ul > li > a').attr('href') + '&fp=945d4e52764ab9b1ce7a8fba0bb8d68d&ip=160.177.72.111'
+    return {
+      img,
+      developer,
+      appname,
+      link
+    }
   }
 }
 
@@ -90,25 +95,33 @@ let apk_dl = {
 
 let aptoide = {
   search: async function(args) {
-     let res = (await fetch(tools.api(5, '/apps/search', {
+    let res = (await fetch(tools.api(5, '/apps/search', {
       query: args,
-      limit:1000
+      limit: 1000
     })))
 
     let ress = {}
     res = (await res.json())
-    ress =   res.datalist.list.map(v=>{
-      return {nama:v.name,id:v.package}
+    ress = res.datalist.list.map(v => {
+      return {
+        nama: v.name,
+        id: v.package
+      }
     })
     return ress
   },
   download: async function(id) {
     let res = (await fetch(tools.api(5, '/apps/search', {
       query: id,
-      limit:1
+      limit: 1
     })))
-    
+
     res = (await res.json())
-      return {img:res.datalist.list[0].icon ,developer:res.datalist.list[0].store.name ,appname:res.datalist.list[0].name ,link:res.datalist.list[0].file.path}
+    return {
+      img: res.datalist.list[0].icon,
+      developer: res.datalist.list[0].store.name,
+      appname: res.datalist.list[0].name,
+      link: res.datalist.list[0].file.path
+    }
   }
 }
